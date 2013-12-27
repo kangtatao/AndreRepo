ad44c2f13af6b9ccf0743e4a91fc86f0aa96bc91
andreluis034 <andrebrandao@sapo.pt>
2013-12-27 23:57:12 +0000

    Update Copy Pasterino(1.1).lua

--- Copy Pasterino(1.1).lua
+++ Copy Pasterino(1.1).lua
@@ -12,11 +12,11 @@
 
 
 function OnLoad()
-    CopyPasterinoConfig = scriptConfig("Copy Pasterino 1.0","Copy Pasterino")
+    CopyPasterinoConfig = scriptConfig("Copy Pasterino 1.1","Copy Pasterino")
 	CopyPasterinoConfig:addParam("sep", "--- Keys ---", SCRIPT_PARAM_INFO, "")
-	CopyPasterinoConfig:addParam("tchat", "Team Chat", SCRIPT_PARAM_ONKEYDOWN, false, 103)
-	CopyPasterinoConfig:addParam("achat", "All chat", SCRIPT_PARAM_ONKEYDOWN, false, 100)
-	PrintChat("The Copy Pasterino 1.0")
+	CopyPasterinoConfig:addParam("tchat", "Team Chat", SCRIPT_PARAM_ONKEYDOWN, false, 103) --Default Numpad 7
+	CopyPasterinoConfig:addParam("achat", "All chat", SCRIPT_PARAM_ONKEYDOWN, false, 100) --Default Numpad 4
+	PrintChat("The Copy Pasterino 1.1 has been loaded")
 	end
 	
 function OnWndMsg(msg, wParam)
