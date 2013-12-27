35066da59d85794b25467ad83f1f2f61507a2eab
andreluis034 <andrebrandao@sapo.pt>
2013-12-27 00:24:40 +0000

    Create Copy Pasterino.lua

---
+++ Copy Pasterino.lua
@@ -0,0 +1,27 @@
+-- [[the Copy pasterino 1.0]] --
+
+
+--Have you ever wanted to CopyPaste something but noticed you can't ctrl+v in-game? Well those days are over, download this script with customizable keys
+--Features:
+--No delay on copy pasterino
+--Has function to paste into all chat or just to team
+
+
+function OnLoad()
+	CopyPasterinoConfig = scriptConfig("Copy Pasterino 1.0","Copy Pasterino")
+	CopyPasterinoConfig:addParam("sep", "--- Keys ---", SCRIPT_PARAM_INFO, "")
+	CopyPasterinoConfig:addParam("tchat", "Team Chat", SCRIPT_PARAM_ONKEYDOWN, false, 103)
+	CopyPasterinoConfig:addParam("achat", "All chat", SCRIPT_PARAM_ONKEYDOWN, false, 100)
+	PrintChat("The Copy Pasterino 1.0")
+	end
+	
+function OnWndMsg(msg, wParam)
+    if CopyPasterinoConfig.tchat == true then
+        pasterino = GetClipboardText()
+        SendChat(pasterino)
+  end
+		if CopyPasterinoConfig.achat == true then
+        pasterino = GetClipboardText()
+        SendChat("/all " .. pasterino)
+				end
+end
