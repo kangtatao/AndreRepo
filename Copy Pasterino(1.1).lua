36e08147f20a9a10e8ea36db115ab3fc5330870e
andreluis034 <andrebrandao@sapo.pt>
2014-01-05 03:43:13 +0000

    Update Copy Pasterino(1.1).lua

--- Copy Pasterino(1.1).lua
+++ Copy Pasterino(1.1).lua
@@ -12,26 +12,26 @@
 
 
 function OnLoad()
-    CopyPasterinoConfig = scriptConfig("Copy Pasterino 1.1","Copy Pasterino")
+	CopyPasterinoConfig = scriptConfig("Copy Pasterino 1.1","Copy Pasterino")
 	CopyPasterinoConfig:addParam("sep", "--- Keys ---", SCRIPT_PARAM_INFO, "")
 	CopyPasterinoConfig:addParam("tchat", "Team Chat", SCRIPT_PARAM_ONKEYDOWN, false, 103) --Default Numpad 7
 	CopyPasterinoConfig:addParam("achat", "All chat", SCRIPT_PARAM_ONKEYDOWN, false, 100) --Default Numpad 4
 	PrintChat("The Copy Pasterino 1.1 has been loaded")
-	end
-	
+end
+
 function OnWndMsg(msg, wParam)
-    if CopyPasterinoConfig.tchat == true then
-        pasterino = GetClipboardText()
-            if pasterino ~= "" then      
-        SendChat(pasterino) 
-					else PrintChat("Please Copy something into clipboard")
-  end
+		if CopyPasterinoConfig.tchat == true then
+		pasterino = GetClipboardText()
+		if pasterino ~= "" then      
+			SendChat(pasterino) 
+			else PrintChat("Please Copy something into clipboard")
+		end
 	end
-		if CopyPasterinoConfig.achat == true then
-        pasterino = GetClipboardText()
-						if pasterino ~= "" then
-        SendChat("/all " .. pasterino)
-							else PrintChat("Please Copy something into the Clipboard")
-				end
-				end
+	if CopyPasterinoConfig.achat == true then
+		pasterino = GetClipboardText()
+		if pasterino ~= "" then
+			SendChat("/all " .. pasterino)
+			else PrintChat("Please Copy something into the Clipboard")
+		end
+	end
 end
